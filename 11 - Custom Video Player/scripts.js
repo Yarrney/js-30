f2c510dc8a2018c51cbbafb5d1b3bd996c314c23
Yarrney <alex.arney@gmail.com>
2017-02-06 17:53:05 +1100

    created updateButton functin for pause/play button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -14,9 +14,19 @@ function togglePlay() {
   // } else {
   //   video.pause();
   // }
-  // The code below is the same as above but using a ternerary operator 
+  // The code below is the same as above but using a ternerary operator
   const method = video.paused ? 'play' : 'pause';
   video[method]();
-};
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚'; // bound to video element
+  toggle.textContent = icon;
+}
 
 // 3. Set up event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
