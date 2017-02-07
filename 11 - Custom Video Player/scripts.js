1c2d4a9260ace7144b14eee65658032975e41a22
Yarrney <alex.arney@gmail.com>
2017-02-07 19:01:38 +1100

    created play pause function on spacebar keydown

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,8 +4,8 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 // 2. Build functions
 function togglePlay() {
@@ -19,14 +19,29 @@ function togglePlay() {
   video[method]();
 }
 
+function spacePlay(e) {
+  if ((e || window.event).keyCode === 32) {
+    togglePlay();
+  }
+}
+
 function updateButton() {
   const icon = this.paused ? '►' : '❚ ❚'; // bound to video element
   toggle.textContent = icon;
 }
 
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip); //parseFloat() modifies string to float (number)
+}
+
 // 3. Set up event listeners
+document.addEventListener('keydown', spacePlay);
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
