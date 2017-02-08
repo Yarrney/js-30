9e69c67c93aee68764227d22a043aefabe16fbdb
Yarrney <alex.arney@gmail.com>
2017-02-08 18:39:34 +1100

    created fullscreen on doubleclick function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -19,20 +19,53 @@ function togglePlay() {
   video[method]();
 }
 
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚'; // bound to video element
+  toggle.textContent = icon;
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration ;
+  video.currentTime = scrubTime;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip); //parseFloat() modifies string to float (number)
+}
+
 function spacePlay(e) {
   if ((e || window.event).keyCode === 32) {
     togglePlay();
   }
 }
 
-function updateButton() {
-  const icon = this.paused ? '►' : '❚ ❚'; // bound to video element
-  toggle.textContent = icon;
-}
-
-function skip() {
-  console.log(this.dataset.skip);
-  video.currentTime += parseFloat(this.dataset.skip); //parseFloat() modifies string to float (number)
+function toggleFullscreen() {
+  if(document.webkitFullscreenElement !== null) {
+    if(document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if(document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if(document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  } else {
+  	if(video.requestFullScreen){
+  		video.requestFullScreen();
+  	} else if(video.webkitRequestFullScreen){
+  		video.webkitRequestFullScreen();
+  	} else if(video.mozRequestFullScreen){
+  		video.mozRequestFullScreen();
+  	}
+  }
 }
 
 // 3. Set up event listeners
@@ -41,7 +74,17 @@ document.addEventListener('keydown', spacePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+video.addEventListener('dblclick', toggleFullscreen);
 
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false );
